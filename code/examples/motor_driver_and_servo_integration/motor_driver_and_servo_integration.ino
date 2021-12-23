8ecef27ea2b08b305619f49d69111d7df8877ec1
Kendrick Nguyen <kendricknguyen@Kendricks-MacBook-Air.local>
2021-12-22 21:12:51 -0800

    motor driver and servo integ example

--- code/examples/motor_driver_and_servo_integration/motor_driver_and_servo_integration.ino
+++ code/examples/motor_driver_and_servo_integration/motor_driver_and_servo_integration.ino
@@ -1,28 +1,49 @@
+// Import Arudino's Servo library
+#include <Servo.h>
+
 #define BAUDRATE 9600
+
+// Pin out for two thumbsticks
+#define thumbstick_x A0
+#define thumbstick_y A1
 
 // Motor pinout
 #define enA 6
 #define in1 7
 #define in2 5
 
-// Pin out for one thumbstick
-#define thumbstick_y A0
+// Sero pinout
+#define servo_pin 10
 
-int raw_analog_reading_y;
+Servo servo;
+
+int raw_analog_reading_x, raw_analog_reading_y;
 
 void setup() {
 
+  // Start Serial Monitor for feedback
   Serial.begin(BAUDRATE);
-  
+
   // Set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+
+  // Attach servo pin to Servo object
+  servo.attach(servo_pin);
   
   // Turn off motors - Initial state
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
+}
 
+void map_servo() {
+
+  // Tweak joystick min and max mapped values as necessary
+  int servo_pos = map(raw_analog_reading_x, 0, 681, 0, 180);
+  
+  servo.write(servo_pos);
+  
 }
 
 void map_motomoto() {
@@ -30,12 +51,12 @@ void map_motomoto() {
   int motor_speed = 255;  // Feel free to tweak. For PWM maximum possible values are 0 to 255
 
   if(raw_analog_reading_y > 352){
-    // Forward. Tweak threshold to +5 of middle/rest state analog reading as necessary
+    // Forward. Tweak threshold to +5 of middle/rest state analog reading
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
   }
   else if(raw_analog_reading_y < 342){
-    // Backward. Tweak threshold to -5 of middle/rest state analog reading as necessary
+    // Backward. Tweak threshold to -5 of middle/rest state analog reading
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
   }
@@ -51,15 +72,23 @@ void map_motomoto() {
 
 void loop() {
 
-  Serial.print("(Y)=");
+  Serial.print("(X,Y)=");
 
   Serial.print("(");
+
+  // Read + print thumbstick X
+  raw_analog_reading_x = analogRead(thumbstick_x);
+  Serial.print(raw_analog_reading_x);
+
+  Serial.print(",");
 
   // Read + print thumbstick Y
   raw_analog_reading_y = analogRead(thumbstick_y);
   Serial.print(raw_analog_reading_y);
 
   Serial.println(")");
+
+  map_servo();
 
   map_motomoto();
   
