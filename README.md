6d911a97bf00144f47bb5a4b46b5bed49da888fd
kendrick010 <nguyen.kendrick@yahoo.com>
2022-08-02 22:42:22 -0700

    updated mcad files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SYWMI_RC_Car
+
+**A project series hosted by Project in a Box at UCSD**
+> Project documentation [@Maker.io](https://www.digikey.com/en/maker/projects/rc-car/a9018adebbd74306b08684a65041a1a6)<br />
+> More about [Project in a Box](https://pibucsd.org/)
+
+## Overview
+Create your own remote controlled (RC) car from scratch with Project in a Box (PiB)! This project is jammed packed with ECE concepts, such as communications, hardware and electronic prototyping, CAD, and programming.
+
+## BOM
+For simplicity, we will split the project's BOM into three:
+- Remote BOM
+- Car (Electronics)
+- Car (Hardware)
+
+Find them [here](https://docs.google.com/spreadsheets/d/1kUkEL7zyoNOz5KWBoTNwHi_Ymp5fQe9DswVCeSJB654/edit#gid=0)
---
+++ code/remote/complete_remote.ino
@@ -0,0 +1,68 @@
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+
+#define BAUDRATE 9600
+#define CE 9
+#define CSN 8
+
+// Pinout for two thumbsticks
+#define thumbstick_x A0
+#define thumbstick_y A1
+
+// Create an RF24 object
+RF24 radio(CE, CSN);
+
+// Unique address through which two modules communicate.
+const byte address[6] = "00001";
+
+int raw_analog_reading_x, raw_analog_reading_y;
+
+void setup() {
+
+  Serial.begin(BAUDRATE);
+
+  radio.begin();
+
+  //set the address
+  radio.openWritingPipe(address);
+
+  //Set module as transmitter
+  radio.stopListening();
+
+}
+
+// Sends the x and y inputs to the car
+void send_joystick_inputs() {
+
+  const int inputs[2] = {raw_analog_reading_x, raw_analog_reading_y};
+
+  radio.write(&inputs, sizeof(inputs));
+
+  delay(10);
+
+}
+
+void loop() {
+
+  Serial.print("(X,Y)=");
+
+  Serial.print("(");
+
+  // Read + print thumbstick X
+  raw_analog_reading_x = analogRead(thumbstick_x);
+  Serial.print(raw_analog_reading_x);
+
+  Serial.print(",");
+
+  // Read + print thumbstick Y
+  raw_analog_reading_y = analogRead(thumbstick_y);
+  Serial.print(raw_analog_reading_y);
+
+  Serial.println(")");
+
+  send_joystick_inputs();
+
+}
